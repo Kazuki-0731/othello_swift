2070b4951dd9cb362c167e1bb9b0e0fee66c2fd1
Kazuki-0731 <ishino.kzk@gmail.com>
2020-07-26 19:39:21 +0900

    Initial Commit

---
+++ UIListApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  UIListApp
+//
+//  Created by Kazuki on 2020/07/26.
+//  Copyright © 2020 susu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UIListAppTests/UIListAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  UIListAppTests.swift
+//  UIListAppTests
+//
+//  Created by Kazuki on 2020/07/26.
+//  Copyright © 2020 susu. All rights reserved.
+//
+
+import XCTest
+@testable import UIListApp
+
+class UIListAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
